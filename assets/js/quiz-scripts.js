bce93b93d84e0a00702ed0a29331227366b0c60e
epecho <dan.epecho@gmail.com>
2017-03-20 14:18:40 -0400

    I finished everything but the timer

--- assets/js/quiz-scripts.js
+++ assets/js/quiz-scripts.js
@@ -1,69 +1,47 @@
-// What we need our script to do
-// 1. Select a question, from the questions array
-// 2. Select the options, for the questions array
-// 3. The User makes a selection presses a button
-// 4. Check to see what button The User pressed
-// 5. Check the value of the button that was pressed agienst answer
-// 6. The User presses the next question button
-// 7. The script loads a new question and options
-// 8. Capture the option The User selected
-// 9. Display score after quiz has been completed
+var currentQuestion = 0;
+var score = 0;
+var totQuestions = questions.length;
 
-/*jslint browser: true*/
-/*global $, jQuery, alert*/
+var container = document.getElementById("quizContainer");
+var questionEl = document.getElementById("question");
+var opt1 = document.getElementById("opt1");
+var opt2 = document.getElementById("opt2");
+var opt3 = document.getElementById("opt3");
+var opt4 = document.getElementById("opt4");
+var nextButton = document.getElementById("nextButton");
+var resultCont = document.getElementById("result");
 
-$(document).ready(function () {
-    "use strict";
-
-    var score = 0,
-        numberOfQuestions = questions.length,
-        //Variabels that are tied to HTML elements
-        containerEl = $("#quizContainer"),
-        questionEl = $("#question"),
-        opt1El = $("#opt1"),
-        opt2El = $("#opt2"),
-        opt3El = $("#opt3"),
-        opt4El = $("#opt4"),
-        // answerToQuestion = $(".option")answer,
-        nextButtonEl = $("#nextButton"),
-        counter = 0;
-
-    function loadQuestion(questionIndex) {
-
-
-        console.log("This is the length of questions I have " + numberOfQuestions);
-
-        var currentQuestion = questions[questionIndex];
-
-        questionEl.html(currentQuestion.question);
-        console.log(currentQuestion.question);
-
-        opt1El.html(currentQuestion.option1);
-        console.log(currentQuestion.option1);
-
-        opt2El.html(currentQuestion.option2);
-        console.log(currentQuestion.option2);
-
-        opt3El.html(currentQuestion.option3);
-        console.log(currentQuestion.option3);
-
-        opt4El.html(currentQuestion.option4);
-        console.log(currentQuestion.option4);
-        
-        // answerToQuestion.html(currentQuestion.option4);
-        // console.log(currentQuestion.option4);
-        
+function loadQuestion(questionIndex) {
+    var q = questions[questionIndex];
+    questionEl.textContent = (questionIndex + 1) + '. ' + q.question;
+    opt1.textContent = q.option1;
+    opt2.textContent = q.option2;
+    opt3.textContent = q.option3;
+    opt4.textContent = q.option4;
 }
-     
-        
-    loadQuestion(0);
 
-    nextButtonEl.on("click", function () {
+function loadNextQuestion() {
+    var selectedOption = document.querySelector("input[type=radio]:checked");
+    if (!selectedOption) {
+        alert("Please select your answer!");
+        return;
+    }
+    var answer = selectedOption.value;
+    if (questions[currentQuestion].answer == answer) {
+        score += 4;
+    }
+    selectedOption.checked = false;
+    currentQuestion++;
+    if (currentQuestion == totQuestions - 1) {
+        nextButtonEl.textContent = "Finish";
+    }
+    if (currentQuestion == totQuestions) {
+        container.style.display = "none";
+        resultCont.style.display = "";
+        resultCont.textContent = "Your Score: " + score;
+        return;
+    }
+    loadQuestion(currentQuestion);
+}
 
-            counter++;
-
-            loadQuestion(counter);
-
-        });
-
-});
+loadQuestion(currentQuestion);
